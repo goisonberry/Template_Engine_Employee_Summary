92f6ee0c2f698913684dfed691bc602be997bb36
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-07-17 12:34:44 -0700

    all tests passed and html generated.

--- app.js
+++ app.js
@@ -1,35 +1,221 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
+const style = require("./templates/css");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
 
-const render = require("./lib/htmlRenderer");
+let finalTeamArray = [];
 
+function startingPrompt() {
+  inquirer
+    .prompt([
+      {
+        message:
+          "/////////Welcome to Team Generator 5000! Please write your team name://///////",
+        name: "teamname",
+      },
+    ])
+    .then(function (data) {
+      const teamName = data.teamname;
+      finalTeamArray.push(teamName);
+      addManager();
+    });
+}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        message: "What is your team manager's name?",
+        name: "name",
+      },
+      {
+        message: "What is your team manager's email address?",
+        name: "email",
+      },
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+      {
+        type: "number",
+        message: "What is your team manager's office number?",
+        name: "officeNumber",
+      },
+    ])
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    .then(function (data) {
+      const name = data.name;
+      const id = 1;
+      const email = data.email;
+      const officeNumber = data.officeNumber;
+      const teamMember = new Manager(name, id, email, officeNumber);
+      finalTeamArray.push(teamMember);
+      addTeamMembers();
+    });
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function addTeamMembers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add more team members?",
+        choices: [
+          "Yes, add an engineer",
+          "Yes, add an intern",
+          "No, my team is complete",
+        ],
+        name: "addMemberData",
+      },
+    ])
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    .then(function (data) {
+      switch (data.addMemberData) {
+        case "Yes, add an engineer":
+          addEngineer();
+          break;
+
+        case "Yes, add an intern":
+          addIntern();
+          break;
+        case "No, my team is complete":
+          compileTeam();
+          break;
+      }
+    });
+}
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        message: "What is this engineer's name?",
+        name: "name",
+      },
+      {
+        message: "What is this engineer's email address?",
+        name: "email",
+      },
+      {
+        message: "What is this engineer's Github profile?",
+        name: "github",
+      },
+    ])
+
+    .then(function (data) {
+      const name = data.name;
+      const id = finalTeamArray.length + 1;
+      const email = data.email;
+      const github = data.github;
+      const teamMember = new Engineer(name, id, email, github);
+      finalTeamArray.push(teamMember);
+      addTeamMembers();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        message: "What is this intern's name?",
+        name: "name",
+      },
+      {
+        message: "What is this intern's email address?",
+        name: "email",
+      },
+      {
+        message: "What is this intern's school?",
+        name: "school",
+      },
+    ])
+
+    .then(function (data) {
+      const name = data.name;
+      const id = finalTeamArray.length + 1;
+      const email = data.email;
+      const school = data.school;
+      const teamMember = new Intern(name, id, email, school);
+      finalTeamArray.push(teamMember);
+      addTeamMembers();
+    });
+}
+
+function compileTeam() {
+  console.log(
+    "//////////You've done it!!! Now give your team a raise.////////"
+  );
+
+  const htmlArray = [];
+  const htmlBeginning = `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${finalTeamArray[0]}</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+     ${style}
+    </style>
+</head>
+
+<body>
+    <div class="banner-bar">
+        <h1>${finalTeamArray[0]}</h1>
+    </div>
+    <div class="card-container">
+    `;
+  htmlArray.push(htmlBeginning);
+
+  for (let i = 1; i < finalTeamArray.length; i++) {
+    let object = `
+        <div class="member-card">
+            <div class="card-top">
+                <h2>${finalTeamArray[i].name}</h2>
+                <h2>${finalTeamArray[i].title}</h2>
+            </div>
+            <div class="card-bottom">
+                <p>Employee ID: ${finalTeamArray[i].id}</p>
+                <p>Email: <a href="mailto:${finalTeamArray[i].email}">${finalTeamArray[i].email}</a>></p>
+        `;
+    if (finalTeamArray[i].officeNumber) {
+      object += `
+            <p>${finalTeamArray[i].officeNumber}</p>
+            `;
+    }
+    if (finalTeamArray[i].github) {
+      object += `
+            <p>GitHub: <a href="https://github.com/${finalTeamArray[i].github}">${finalTeamArray[i].github}</a></p>
+            `;
+    }
+    if (finalTeamArray[i].school) {
+      object += `
+            <p>School: ${finalTeamArray[i].school}</p>
+            `;
+    }
+    object += `
+        </div>
+        </div>
+        `;
+    htmlArray.push(object);
+  }
+
+  const htmlEnd = `
+    </div>
+    </body>
+    </html>
+    `;
+  htmlArray.push(htmlEnd);
+
+  fs.writeFile(
+    `./generate-html/${finalTeamArray[0]}.html`,
+    htmlArray.join(""),
+    function (err) {}
+  );
+}
+
+startingPrompt();
